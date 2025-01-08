35485d3c188e8cee9ae2efbb26c1f6064664b9f7
Trevor Hall <trhall@tableau.com>
2025-01-07 17:41:57 -0800

    sends transcript - too large through need to trim or something

--- podcast/summarizer.py
+++ podcast/summarizer.py
@@ -1,15 +1,20 @@
+import html2text
 import os
+import parsel
+import requests
 from openai import OpenAI
+
 client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY")
 )
+
+h2t = html2text.HTML2Text()
 
 podcasts = [
     {
         "link": "https://twit.tv/posts/transcripts/week-tech-1013-transcript",
         "title": "This Week in Tech 1013 Transcript",
-        "published_date": "Jan 6th 2025",
-        "transcript": "stuff happened"
+        "published_date": "Jan 6th 2025"
     }
 ]
 
@@ -29,16 +34,30 @@ Transcript:
 """
 
 for p in podcasts:
-    messages.append(
-        {
-            "role": "system",
-            "content": template.format(
-                title=p["title"],
-                pub=p["published_date"],
-                transcript=p["transcript"]
-            )
-        }
-    )
+
+    r = requests.get(p["link"])
+    html = r.text
+    parsel_dom = parsel.Selector(text=html)
+    statements = parsel_dom.css("div.body p").getall()
+
+    for s in statements:
+        messages.append(
+            {
+                "role": "system",
+                "content": template.format(
+                    title=p["title"],
+                    pub=p["published_date"],
+                    transcript=h2t.handle(s)
+                )
+            }
+        )
+
+messages.append(
+    {
+        "role": "user",
+        "content": "what is the summary?"
+    }
+)
 
 completion = client.chat.completions.create(
   model="gpt-4o",
