f72698f3b8d71c136de782f684ba0dc3961d2df4
Trevor Hall <trhall@tableau.com>
2025-01-08 16:18:41 -0800

    update to assistant (and works)

--- assistants/math_tutor.py
+++ assistants/math_tutor.py
@@ -1,40 +1,45 @@
 import os
+import pprint
 from openai import OpenAI
 client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY")
 )
 
-######
-# https://platform.openai.com/docs/assistants/quickstart?context=without-streaming
-######
+from openai import OpenAI
+client = OpenAI()
 
 assistant = client.beta.assistants.create(
-    name="Math Tutor",
-    instructions="You are a personal math tutor. Write and run code to answer math questions.",
-    tools=[
-        {
-            "type": "code_interpreter"
-        }
-    ],
+    instructions="""
+    You are an executive assistant. Your job is to help
+    me make my life simpler. Sometimes this is through
+    integrating to applications and automating some
+    things I need via APIs.
+
+    For example, this might be with Google Calendar.
+    I might ask about my day, who is in what meetings,
+    when I have breaks, when my day starts, along with
+    helping to create meetings and other tasks.
+    """,
+    name="Executive Assistant",
+    tools=[{"type": "code_interpreter"}],
     model="gpt-4o",
 )
 
 thread = client.beta.threads.create()
-
 message = client.beta.threads.messages.create(
-    thread_id=thread.id,
+    thread.id,
     role="user",
-    content="I need to solve the equation `3x + 11 = 14`. Can you help me?"
+    content="What's my day look like today?"
 )
 
 run = client.beta.threads.runs.create_and_poll(
-    thread_id=thread.id,
-    assistant_id=assistant.id,
-    instructions="Please address the user as Jane Doe. The user has a premium account."
+  thread_id=thread.id,
+  assistant_id=assistant.id,
+  instructions="This is a Google Calendar task"
 )
 
 if run.status == 'completed':
     messages = client.beta.threads.messages.list(thread_id=thread.id)
-    print(messages)
+    pprint.pp(messages)
 else:
     print(run.status)
